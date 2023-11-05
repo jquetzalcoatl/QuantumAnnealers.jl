2fbc0bf68da0217e1ca9d3fad6735d9678b7a7b6
Javier Toledo <Javier.Toledo1@bcchr.ca>
2023-11-05 12:22:19 -0800

    simple usecase examples

--- src/QuantumAnnealers.jl
+++ src/QuantumAnnealers.jl
@@ -17,11 +17,25 @@ function solve_qubo(solver::String, Q::Dict{Int, Dict{Int, Float64}}, params::Di
     return fetch_results(solver, job_id)
 end
 
-function fetch_results(solver::String, job_id::String)
-    url = "$BASE_URL/solvers/$solver/jobs/$job_id/"
-    response = HTTP.get(url, headers)
-    result = JSON.parse(String(response.body))
-    return result
+# function fetch_results(solver::String, job_id::String)
+#     url = "$BASE_URL/solvers/$solver/jobs/$job_id/"
+#     response = HTTP.get(url, headers)
+#     result = JSON.parse(String(response.body))
+#     return result
+# end
+
+# function ising_sample(solver::String, h::Dict{Int,Float64}, J::Dict{Tuple{Int,Int},Float64}, num_reads::Int=100)
+function ising_sample(solver::String, h, J, num_reads::Int=100)
+    url = "$(BASE_URL)/solvers/$(solver)/jobs/"
+    body = JSON.json(Dict("type" => "ising", "linear" => h, "quadratic" => J, "num_reads" => num_reads))
+    response = HTTP.post(url, body=body)
+    return JSON.parse(String(response.body))["id"]
+end
+
+function fetch_results(job_id::String)
+    url = "$(BASE_URL)/jobs/$(job_id)/"
+    response = HTTP.get(url, headers=HEADERS)
+    return JSON.parse(String(response.body))
 end
 
 end
